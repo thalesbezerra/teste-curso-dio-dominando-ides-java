7fba43187d5790ef63961f0c608a8dc2034cd76b
thalesbezerra <bezerra.thales@gmail.com>
2022-02-13 14:09:16 -0300

    Terceira alteração feita por thales

--- src/br/com/thalesbezerra/git/PrimeiroPrograma.java
+++ src/br/com/thalesbezerra/git/PrimeiroPrograma.java
@@ -10,6 +10,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
 		System.out.println("Segunda alteração!");
+		System.out.println("Terceira alteração!");
 
 	}
 
