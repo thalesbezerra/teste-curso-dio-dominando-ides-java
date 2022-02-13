d8ef676ce141745cef616db03cbe4072b046c8da
thalesbezerra <bezerra.thales@gmail.com>
2022-02-13 13:55:50 -0300

    frist commit

---
+++ src/br/com/thalesbezerra/git/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+
+package br.com.thalesbezerra.git;
+
+/**
+ * @author adm_rededor
+ *Integrando o IDE Eclipse com o GitHub
+ */
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub!");
+
+	}
+
+}
